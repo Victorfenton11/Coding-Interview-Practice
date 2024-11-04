6c3466a566cb3e13c98d3a0fe27559222b867fb1
Victorfenton11 <victorfenton.a@hotmail.com>
2024-11-04 10:38:15 -0500

    Finish LinkedList implementation

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -7,6 +7,14 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
+    def length(self):
+        curr = self.head
+        i = 0
+        while curr is not None:
+            i += 1
+            curr = curr.next
+        return i
+    
     def search(self, value):
         curr = self.head
         i = 0
@@ -29,10 +37,26 @@ class LinkedList:
 
     def insertAtBeginning(self, value):
         newNode = Node(value)
-        if not self.head:
-            self.head = Node(value)
+        newNode.next = self.head
+        self.head = newNode
+        return
+    
+    def insertAtIndex(self, value, index):
+        if index == 0:
+            self.insertAtBeginning(value)
             return
-        
+
+        curr = self.head
+        i = 0
+        while curr is not None:
+            if i == index - 1:
+                newNode = Node(value)
+                newNode.next = curr.next
+                curr.next = newNode
+                return
+            curr = curr.next
+            i += 1
+        raise IndexError("Index out of range")
     
     def remove(self, value):
         curr = self.head
@@ -78,8 +102,10 @@ llist.insertAtEnd(4)
 print(llist)
 llist.insertAtEnd(6)
 print(llist)
+print(llist.length())
 llist.remove(1)
 print(llist)
+print(llist.length())
 llist.insertAtEnd(5)
 print(llist)
 print(llist.search(5))
@@ -87,3 +113,11 @@ llist.insertAtEnd(3)
 llist.insertAtEnd(230)
 llist.reverse()
 print(llist)
+print(llist.length())
+llist.insertAtIndex(11, 0)
+print(llist)
+llist.insertAtIndex(12, 3)
+print(llist)
+llist.insertAtIndex(13, 7)
+print(llist)
+llist.insertAtIndex(14, 9)
